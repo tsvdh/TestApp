056f38161c7f09af8ec8e0579cee4c30592d7285
Tobias van den Hurk <T.S.vandenHurk@student.tudelft.nl>
2021-09-18 22:01:43 -0600

    fix name in properties

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
 
         @Override
         public void start() {
-            new Message("This an app to test Basket\n v1.0.1", false);
+            new Message("This an app to test Basket\n v1.0.2", false);
         }
 
         public static void invokeStart() {
